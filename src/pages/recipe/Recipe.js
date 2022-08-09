a2ff765ac0eb917c5a60d3ddcaf97ae273ab53ad
Amy Chan <amy.chansya@gmail.com>
2022-08-09 10:53:50 -0700

    Fetch recipe from db for Recipe component

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,10 +1,32 @@
 // styles
 import './Recipe.css'
 
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
+
+// extract route parameter, then fetch data using useFetch hook
 export default function Recipe() {
+  const { id } = useParams()
+  const url = 'http://localhost:3000/recipes/' + id
+
+  const { data: recipe, isPending, error } = useFetch(url)
+
+  
   return (
-    <div>
-      Recipe
+    <div className='recipe'>
+      {isPending && <p className='loading'>Loading...</p>}
+      {error && <p className='error'>{error}</p>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map(ing=> <li key={ing}>{ing}</li>)}
+          </ul>
+          <p className='method'>{recipe.method}</p>
+        </>
+
+        )}
     </div>
   )
 }

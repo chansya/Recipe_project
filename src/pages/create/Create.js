969babbe94dcb89cec241c6b0a06488fd00f6c05
Amy Chan <amy.chansya@gmail.com>
2022-08-10 08:59:53 -0700

    Add create recipe form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,9 +1,55 @@
+import { useState } from 'react'
+
 // styles
 import './Create.css'
 
+export default function Create() {
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [cookingTime, setCookingTime] = useState('')
 
-export default function create() {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(title,method,cookingTime)
+  }
+
   return (
-    <div>create</div>
+    <div className='create'>
+        <h2 className="page-title">Add a New Recipe</h2>
+        
+        <form onSubmit={handleSubmit}>
+            <label>
+                <span>Recipe title:</span>
+                <input 
+                    type="text" 
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                    required
+                />
+            </label>
+
+            {/* Ingredients here */}
+
+            <label>
+                <span>Recipe method:</span>
+                <textarea
+                    onChange={(e)=> setMethod(e.target.value)}
+                    value={method}
+                    required
+                />
+            </label>
+            <label>
+                <span>Cooking time (minutes):</span>
+                <input 
+                    type="number" 
+                    onChange={(e) => setCookingTime(e.target.value)}
+                    value={cookingTime}
+                    required
+                />
+            </label>
+            <button className="btn">Submit</button>
+        </form>
+
+    </div>
   )
 }
